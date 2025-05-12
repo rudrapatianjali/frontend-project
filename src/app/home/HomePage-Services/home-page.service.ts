02d13cb8782f7010a89477438c739f6daf82c08f
rudrapatianjali <132752588+rudrapatianjali@users.noreply.github.com>
2025-05-12 13:16:29 +0530

    Update home-page.service.ts

--- src/app/home/HomePage-Services/home-page.service.ts
+++ src/app/home/HomePage-Services/home-page.service.ts
@@ -29,7 +29,7 @@ export class HomePageService {
   
     console.log(emailID)
 
-    let tempVar = this.http.get<boolean>('https://quickkart-backend.azurewebsites.net/api/customer/AddNewSubscriber?emailID='+emailID)
+    let tempVar = this.http.get<boolean>('https://backendwebapp-h9a5ebc0gsbqaegn.westus-01.azurewebsites.net/api/customer/AddNewSubscriber?emailID='+emailID)
     console.log(tempVar)
     return tempVar
   }
